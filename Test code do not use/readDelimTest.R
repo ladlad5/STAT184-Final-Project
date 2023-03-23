984322c06713a6aa05d0f963c998ecf2ef3444f1
ladlad5 <gilad.stessel@gmail.com>
2023-03-23 14:52:37 -0400

    converting python data into readable R and plot working

---
+++ Test code do not use/readDelimTest.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(tidyverse)
+speedData <- scan("lec_car_data.txt", character(), quote = "")
+timeData <- scan("lec_time_data.txt", character(), quote = "")
+
+lapData <- data.table(timeData, speedData)
+
+lapData$timeData <- as.numeric(lapData$timeData)
+lapData$timeData <- lapData$timeData / 1000
+lapData$speedData <- as.numeric(lapData$speedData)
+
+ggplot(lapData, aes(x = timeData, y = speedData)) +
+  geom_line(color = "red", size = 1.5) +
+  labs(x = "Time (s)", y = "Speed (KPH)", title = "Leclerc Qualifying Lap Speed") +
+  ylim(0, 370) +
+  theme_minimal()
+
+
+
