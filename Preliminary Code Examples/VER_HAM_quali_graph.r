972f488ad7c6ab09fba2cf7862c3617192badc5a
ladlad5 <gilad.stessel@gmail.com>
2023-04-10 19:54:35 -0400

    graphing works

--- Preliminary Code Examples/VER_HAM_quali_graph.r
+++ Preliminary Code Examples/VER_HAM_quali_graph.r
@@ -24,3 +24,25 @@ VER2022LapData <- data.table(VER2022TimeData, VER2022SpeedData)
 VER2022LapData$VER2022TimeData <- as.numeric(VER2022LapData$VER2022TimeData)
 VER2022LapData$VER2022TimeData <- VER2022LapData$VER2022TimeData / 1000
 VER2022LapData$VER2022SpeedData <- as.numeric(VER2022LapData$VER2022SpeedData)
+
+ggplot() +
+  geom_line(data = HAM2021LapData, aes(x = HAM2021TimeData, y = HAM2021SpeedData, color = "Hamilton")) +
+  geom_line(data = VER2021LapData, aes(x = VER2021TimeData, y = VER2021SpeedData, color = "Verstappen")) +
+  labs(title = "2021 Lap Time and Speed Comparison",
+       x = "Lap Time (s)", y = "Speed (km/h)",
+       color = "Driver") +
+  scale_color_manual(values = c("red", "blue"),
+                     labels = c("Hamilton", "Verstappen"),
+                     name = "Driver") +
+  theme_classic()
+
+ggplot() +
+  geom_line(data = HAM2022LapData, aes(x = HAM2022TimeData, y = HAM2022SpeedData, color = "Hamilton")) +
+  geom_line(data = VER2022LapData, aes(x = VER2022TimeData, y = VER2022SpeedData, color = "Verstappen")) +
+  labs(title = "2022 Lap Time and Speed Comparison",
+       x = "Lap Time (s)", y = "Speed (km/h)",
+       color = "Driver") +
+  scale_color_manual(values = c("red", "blue"),
+                     labels = c("Hamilton", "Verstappen"),
+                     name = "Driver") +
+  theme_classic()
