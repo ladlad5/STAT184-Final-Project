555b0aee12e13f28ef8cace3e85eae50a1d15194
ladlad5 <gilad.stessel@gmail.com>
2023-04-11 15:24:56 -0400

    parsed need to write to csv

--- Preliminary Code Examples/Monza_2022_Race_Data.py
+++ Preliminary Code Examples/Monza_2022_Race_Data.py
@@ -1,6 +1,22 @@
 import fastf1
+import numpy as np
 import pandas
+import csv
 fastf1.Cache.enable_cache('cache')
 session = fastf1.get_session(2022, 'Monza', 'R')
 
 session.load()
+lapDataHAM = session.laps.pick_driver('HAM')
+lapDataVER = session.laps.pick_driver('VER')
+HAMLapTimes = lapDataHAM['LapTime'].tolist()
+HAMLapTimes = HAMLapTimes[1:]
+for x in range(len(HAMLapTimes)):
+    HAMLapTimes[x] = str(HAMLapTimes[x].to_numpy().astype('timedelta64[ms]'))
+    HAMLapTimes[x] = HAMLapTimes[x].replace(' milliseconds', '')
+VERLapTimes = lapDataVER['LapTime'].tolist()
+VERLapTimes = VERLapTimes[1:]
+for x in range(len(VERLapTimes)):
+    VERLapTimes[x] = str(VERLapTimes[x].to_numpy().astype('timedelta64[ms]'))
+    VERLapTimes[x] = VERLapTimes[x].replace(' milliseconds', '')
+HAMCompound = lapDataHAM['Compound'].tolist()
+VERCompound = lapDataVER['Compound'].tolist()
